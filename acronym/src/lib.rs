7bcea22f63831f978410a7647b202e9cc4e9f7bf
alexey-mz <knut020@gmail.com>
2020-09-24 01:03:09 +0300

    acronym done

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    let t = phrase.split(|ch: char| !ch.is_alphabetic()).filter(|&x| !x.is_empty()).filter(|&x| x.len() > 1 || x.chars().next().unwrap().is_uppercase()).collect::<Vec<&str>>();
+    let res = t.iter().fold(String::new(), |mut acc, &x| {
+        if x.chars().all(|x| x.is_uppercase()) {
+            acc.push(x.chars().next().unwrap());
+        }
+        else if (x.chars().filter(|ch| ch.is_uppercase()).collect::<String>()).len() > 1 {
+            acc.push_str(x.chars().filter(|x| x.is_uppercase()).collect::<String>().as_str())
+        }
+        else {
+            acc.push(x.chars().next().unwrap().to_ascii_uppercase());
+        }
+        acc
+    });
+    res
 }
--- test_sieve/src/main.rs
+++ test_sieve/src/main.rs
@@ -1,18 +1,47 @@
-use factorial::Factorial;
-
-fn elem (row: u32, elem: u32) -> u32 {
-    row.factorial() / (elem.factorial() * (row - elem).factorial())
-}
-
-fn pascal_row (row: u32) -> Vec<u32> {
-    (0..=row).into_iter().map(|x| elem(row, x)).collect::<Vec<u32>>()
-}
-
-
+// pub fn abbreviate(phrase: &str) -> String {
+//     // phrase.chars().into_iter().filter(|x| x.is_uppercase()).collect::<String>()
+//     // let count_uppercase = phrase.chars().into_iter().fold(0, |mut acc, x| {
+//     //     if x.is_uppercase() {
+//     //         acc += 1
+//     //     }
+//     //     acc
+//     // });
+//     // let count_words = phrase.split(' ').collect::<Vec<&str>>();
+//     let check_alphabetic_only = phrase.chars().into_iter().all(|x| x.is_alphabetic());
+//
+//     let punctuations = phrase.chars().into_iter().filter(|x| x.is_ascii_punctuation()).collect::<String>();
+//
+//     match check_alphabetic_only {
+//         true => {
+//             phrase.chars().into_iter().filter(|x| x.is_uppercase()).collect::<String>()
+//         },
+//         false => {
+//
+//         }
+//     }
+// }
 
 fn main() {
-    let n = elem(0, 0);
-    let n_row = pascal_row(3);
-    let result: Vec<_> = (0..3u32).into_iter().map(|x| pascal_row(x)).collect();
-    println!("{:?}", result);
+    let phrase = "PHP: Hypertext Preprocessor";
+    // let punctuations = phrase.chars().into_iter().filter(|x| x.is).collect::<String>();
+    // println!("{}", punctuations);
+    let test = "HyperText Markup Language";
+    println!("{:?}",test.split(|ch: char| !ch.is_alphabetic()).filter(|&x| !x.is_empty()).collect::<Vec<&str>>());
+    let t = test.split(|ch: char| !ch.is_alphabetic()).filter(|&x| !x.is_empty()).filter(|&x| x.len() > 1 || x.chars().next().unwrap().is_uppercase()).collect::<Vec<&str>>();
+    let res = t.iter().fold(String::new(), |mut acc, &x| {
+        if x.chars().all(|x| x.is_uppercase()) {
+            acc.push(x.chars().next().unwrap());
+        }
+        else if (x.chars().filter(|ch| ch.is_uppercase()).collect::<String>()).len() > 1 {
+
+            acc.push_str(x.chars().filter(|x| x.is_uppercase()).collect::<String>().as_str())
+        }
+        else {
+            acc.push(x.chars().next().unwrap().to_ascii_uppercase());
+        }
+        acc
+    });
+    println!("{:?}", t);
+
+    println!("{:?}", res);
 }
