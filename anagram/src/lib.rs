37ec739eb3f54de698ea8de7b32bc42286409bd8
alexey-mz <knut020@gmail.com>
2020-09-25 01:19:04 +0300

    anagram done

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,23 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+fn compare_words(a: &str, b: &str) -> bool {
+    let mut word1 = a.to_lowercase().chars().collect::<Vec<char>>();
+    word1.sort();
+    let mut word2 = b.to_lowercase().chars().collect::<Vec<char>>();
+    word2.sort();
+    word1 == word2
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    possible_anagrams.iter().fold(HashSet::new(), |mut acc, &word_from_iter| {
+        if word_from_iter.to_lowercase() == word.to_lowercase() {
+            ()
+        }
+        else {
+            if compare_words(word, word_from_iter) {
+                acc.insert(word_from_iter);
+            }
+        }
+        acc
+    })
 }
--- test_sieve/src/main.rs
+++ test_sieve/src/main.rs
@@ -1,47 +1,16 @@
-// pub fn abbreviate(phrase: &str) -> String {
-//     // phrase.chars().into_iter().filter(|x| x.is_uppercase()).collect::<String>()
-//     // let count_uppercase = phrase.chars().into_iter().fold(0, |mut acc, x| {
-//     //     if x.is_uppercase() {
-//     //         acc += 1
-//     //     }
-//     //     acc
-//     // });
-//     // let count_words = phrase.split(' ').collect::<Vec<&str>>();
-//     let check_alphabetic_only = phrase.chars().into_iter().all(|x| x.is_alphabetic());
-//
-//     let punctuations = phrase.chars().into_iter().filter(|x| x.is_ascii_punctuation()).collect::<String>();
-//
-//     match check_alphabetic_only {
-//         true => {
-//             phrase.chars().into_iter().filter(|x| x.is_uppercase()).collect::<String>()
-//         },
-//         false => {
-//
-//         }
-//     }
-// }
+use std::collections::HashSet;
+
+fn compare_words(a: &str, b: &str) -> bool {
+    let mut word1 = a.chars().collect::<Vec<char>>();
+    word1.sort();
+    let mut word2 = b.chars().collect::<Vec<char>>();
+    word2.sort();
+    word1 == word2
+}
 
 fn main() {
-    let phrase = "PHP: Hypertext Preprocessor";
-    // let punctuations = phrase.chars().into_iter().filter(|x| x.is).collect::<String>();
-    // println!("{}", punctuations);
-    let test = "HyperText Markup Language";
-    println!("{:?}",test.split(|ch: char| !ch.is_alphabetic()).filter(|&x| !x.is_empty()).collect::<Vec<&str>>());
-    let t = test.split(|ch: char| !ch.is_alphabetic()).filter(|&x| !x.is_empty()).filter(|&x| x.len() > 1 || x.chars().next().unwrap().is_uppercase()).collect::<Vec<&str>>();
-    let res = t.iter().fold(String::new(), |mut acc, &x| {
-        if x.chars().all(|x| x.is_uppercase()) {
-            acc.push(x.chars().next().unwrap());
-        }
-        else if (x.chars().filter(|ch| ch.is_uppercase()).collect::<String>()).len() > 1 {
 
-            acc.push_str(x.chars().filter(|x| x.is_uppercase()).collect::<String>().as_str())
-        }
-        else {
-            acc.push(x.chars().next().unwrap().to_ascii_uppercase());
-        }
-        acc
-    });
-    println!("{:?}", t);
-
-    println!("{:?}", res);
+    let word = "good";
+    let word2 = "dog";
+    println!("{:?}", compare_words(word, word2));
 }
